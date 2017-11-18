fcbb821cc518d3155be9a3bb5458a8ef5bbc737b
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-18 13:11:29 +0700

    menambahkan list kategori

---
+++ SampleASPWebsite/Controllers/KategoriController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using Dapper;
+using System.Configuration;
+using SampleASPWebsite.Models;
+
+namespace SampleASPWebsite.Controllers
+{
+    public class KategoriController : Controller
+    {
+        public string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        }
+
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            List<Kategori> listKategori;
+            using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+            {
+                string strSql = @"select * from Kategori order by KategoriName asc";
+                listKategori = conn.Query<Kategori>(strSql).ToList();
+            }
+            return View(listKategori);
+        }
+
+        // GET: Kategori/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Kategori/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kategori/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Kategori/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Kategori/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Kategori/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Kategori/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
