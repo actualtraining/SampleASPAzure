059baf40591dd26e6e2dd42e198a164cf6fd52c5
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-18 12:58:31 +0700

    Add project files.

---
+++ SampleASPWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleASPWebsite.MvcApplication" Language="C#" %>
---
+++ SampleASPWebsite/Models/Kategori.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleASPWebsite.Models
+{
+    public class Kategori
+    {
+        public int KategoriID { get; set; }
+        public string KategoriName { get; set; }
+    }
+}
